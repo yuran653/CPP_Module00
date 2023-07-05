4c12d0ae37f7d5bf31c18c76053c43d50f4c853f
Iurii Ovsiannikov <jgoldste@z3t10c2.42bangkok.com>
2023-07-05 20:25:57 +0700

    ex00_done

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jgoldste <jgoldste@student.42bangkok.co    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/05 18:17:46 by jgoldste          #+#    #+#             */
+/*   Updated: 2023/07/05 20:12:22 by jgoldste         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <cctype>
+
+int main(int argc, char **argv)
+{
+	if(argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	else
+		for (int i = 1; argv[i]; i++)
+			for (int j = 0; argv[i][j]; j++)
+				std::cout << static_cast<const char>(std::toupper(argv[i][j]));
+			std::cout << std::endl;
+	return 0;
+}
