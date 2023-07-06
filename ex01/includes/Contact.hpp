01b94adde9f63e8ffa70e41f11063a8371254cb5
Iurii Ovsiannikov <jgoldste@z3t10c2.42bangkok.com>
2023-07-06 18:17:18 +0700

    ex01_Makefile_done

--- ex01/includes/Contact.hpp
+++ ex01/includes/Contact.hpp
@@ -6,7 +6,7 @@
 /*   By: jgoldste <jgoldste@student.42bangkok.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/06 12:59:55 by jgoldste          #+#    #+#             */
-/*   Updated: 2023/07/06 14:21:02 by jgoldste         ###   ########.fr       */
+/*   Updated: 2023/07/06 18:13:08 by jgoldste         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,11 @@
 #define CONTACT_HPP
 
 #include <iostream>
+
+class Contact
+{
+	private:
+		first name, last name, nickname, phone number, and darkest secret
+}
 
 #endif
