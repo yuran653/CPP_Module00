6394550b9950569c737a76221cc2a0d568acb019
Iurii Ovsiannikov <jgoldste@z3t10c2.42bangkok.com>
2023-07-06 15:18:12 +0700

    ex00_is_done

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,32 +6,24 @@
 /*   By: jgoldste <jgoldste@student.42bangkok.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/05 18:17:46 by jgoldste          #+#    #+#             */
-/*   Updated: 2023/07/05 22:15:09 by jgoldste         ###   ########.fr       */
+/*   Updated: 2023/07/06 12:51:33 by jgoldste         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <cctype>
-#include <string>
 
 int main(int argc, char **argv)
 {
-	for (int s = 0; argv[s]; s++)
-		std::cout << argv[s] << std::endl;
-	std::cout << std::endl << "================" << std::endl << std::endl;
 	if(argc == 1)
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
 	else
-	{
 		for (int i = 1; argv[i]; i++)
 			for (int j = 0; argv[i][j]; j++)
-			{
 				if(std::isalpha(argv[i][j]))
 					std::cout << static_cast<const char>(std::toupper(argv[i][j]));
 				else
 					std::cout << argv[i][j];
-			}
 		std::cout << std::endl;
-	}
 	return 0;
 }
--- ex01/includes/Contact.hpp
+++ ex01/includes/Contact.hpp
@@ -1,20 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   phonebook.cpp                                      :+:      :+:    :+:   */
+/*   Contact.hpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jgoldste <jgoldste@student.42bangkok.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/07/05 21:40:38 by jgoldste          #+#    #+#             */
-/*   Updated: 2023/07/06 13:01:56 by jgoldste         ###   ########.fr       */
+/*   Created: 2023/07/06 12:59:55 by jgoldste          #+#    #+#             */
+/*   Updated: 2023/07/06 14:21:02 by jgoldste         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "PhoneBook.hpp"
-#include "Contact.hpp"
+#ifndef CONTACT_HPP
+#define CONTACT_HPP
 
-int main()
-{
-	std::cout << "TEST TEST TEST" << std::endl;
-	return 0;
-}
+#include <iostream>
+
+#endif
